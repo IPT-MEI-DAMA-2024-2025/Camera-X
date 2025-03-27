ae54526a3d11fbf6ec3dc1ea2066133b2ae54ced
jcnpereira <casimiro@ipt.pt>
2025-03-27 21:54:54 +0000

    capturar e guardar uma fotografia

--- app/src/main/java/pt/ipt/dama/camerax/MainActivity.kt
+++ app/src/main/java/pt/ipt/dama/camerax/MainActivity.kt
@@ -1,21 +1,29 @@
 package pt.ipt.dama.camerax
 
 import android.Manifest
+import android.content.ContentValues
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import pt.ipt.dama.camerax.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.Locale
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
-import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
 
@@ -68,22 +76,101 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
+    /**
+     * 'tira' a fotografia e guarda-a no espaço de armazenamento
+     */
     private fun takePhoto() {
-        TODO("Not yet implemented")
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture
+
+        // Criar o nome a atribuir à imagem, à fotografia.
+        // a este nome irá está associada a data e a hora da captura
+        // import java.text.SimpleDateFormat
+        // import java.util.Locale
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis())
+        // definir o tipo de imagem e onde será guardada
+        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/MIME_types
+        // import android.content.ContentValues
+        // import android.provider.MediaStore
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                // local onde o ficheiro vai ser guardado
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Images")
+            }
+        }
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(
+            contentResolver, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues
+        ).build()
+
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                // import androidx.camera.core.ImageCaptureException
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, getString(R.string.photo_capture_failed, exc.message), exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val msg = getString(R.string.photo_capture_succeeded, output.savedUri)
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_LONG).show()
+                    Log.d(TAG, msg)
+                }
+            })
     }
+
 
     /**
      * inicia a câmara
      */
     private fun startCamera() {
-        TODO("Not yet implemented")
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.surfaceProvider = viewBinding.viewFinder.surfaceProvider
+                }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture
+                )
+
+            } catch (exc: Exception) {
+                Log.e(TAG, getString(R.string.use_case_binding_failed), exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
     }
+
 
     /**
      * devolve 'true' se houve autorização
      * devolve 'false' se foi negada a autorização
      */
-    private fun allPermissionsGranted():Boolean = REQUIRED_PERMISSIONS.all {
+    private fun allPermissionsGranted(): Boolean = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
             baseContext, it
         ) == PackageManager.PERMISSION_GRANTED
@@ -103,7 +190,8 @@ class MainActivity : AppCompatActivity() {
      * e inicia o uso da câmara, ou informa que não é possível utilizá-la
      */
     private val activityResultLauncher =
-        registerForActivityResult(  ActivityResultContracts.RequestMultiplePermissions()
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
         )
         { permissions ->
 
@@ -143,7 +231,13 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
+    /**
+     * por último, destruir o 'gestor da câmara'
+     */
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
 
 
 }
